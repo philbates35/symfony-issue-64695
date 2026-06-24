cc8edf8e33a8ea3eb301e57a015053c92f30a9e0
Phil Bates <philbates35@gmail.com>
2026-06-24 09:10:51 +0100

    Fix

--- public/index.php
+++ public/index.php
@@ -5,5 +5,7 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return static function (array $context) {
+    assert(is_string($context['APP_ENV']));
+
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
